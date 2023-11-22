4492dc57c634ae52d386e508229474a1f10a2414
svnoliveira <srkuran@gmail.com>
2023-11-21 22:02:29 -0300

    feat: criado itens realisticos para os mocks

--- _core/default_data/order_items.py
+++ _core/default_data/order_items.py
@@ -1,68 +1,68 @@
 default_order_items = [
     {
         "order": 1,
-        "product_name": "white shirt",
-        "product_price": 9999.00,
+        "product_name": "Visão Protegida em Tons de Azul",
+        "product_price": 249.99,
         "quantity": 2
     },
     {
         "order": 2,
-        "product_name": "super skirt",
-        "product_price": 9999.00,
+        "product_name": "Elegância Aveludada",
+        "product_price": 249.99,
         "quantity": 1
     },
     {
         "order": 2,
-        "product_name": "blue coat",
-        "product_price": 9999.00,
+        "product_name": "Denim Versátil para Primavera e Verão",
+        "product_price": 199.99,
         "quantity": 1
     },
     {
         "order": 3,
-        "product_name": "black suit",
-        "product_price": 9999.00,
+        "product_name": "Elegância Atemporal: Terno Preto",
+        "product_price": 1349.99,
         "quantity": 4
     },
     {
         "order": 4,
-        "product_name": "leather boots",
-        "product_price": 9999.00,
+        "product_name": "Couro Clássico Masculino",
+        "product_price": 249.99,
         "quantity": 1
     },
     {
         "order": 4,
-        "product_name": "winter dress",
-        "product_price": 9999.00,
+        "product_name": "Estilo Boêmio com Camisa Crochê",
+        "product_price": 299.99,
         "quantity": 1
     },
     {
         "order": 4,
-        "product_name": "black pants",
-        "product_price": 9999.00,
+        "product_name": "Couro Nobre para Outono e Inverno",
+        "product_price": 249.99,
         "quantity": 1
     },
     {
         "order": 4,
-        "product_name": "white shirt",
-        "product_price": 9999.00,
+        "product_name": "Elegância Lilás para Primavera e Verão",
+        "product_price": 249.99,
         "quantity": 1
     },
     {
         "order": 5,
-        "product_name": "white shirt",
-        "product_price": 9999.00,
+        "product_name": "Conforto Casual em Azul Claro",
+        "product_price": 149.99,
         "quantity": 10
     },
     {
         "order": 6,
-        "product_name": "black suit",
-        "product_price": 9999.00,
+        "product_name": "Companheiro de Pulso Executivo",
+        "product_price": 499.99,
         "quantity": 2
     },
     {
         "order": 7,
-        "product_name": "blue coat",
-        "product_price": 9999.00,
+        "product_name": "Chapeu Chic de Inverno",
+        "product_price": 149.99,
         "quantity": 2
     },
 ]
